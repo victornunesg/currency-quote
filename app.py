633afbe91ef36aa23a39e96fccfeb6f8a1704cb0
Victor Nunes <victorngomes@gmail.com>
2023-08-08 15:09:54 -0300

    app updated

--- app.py
+++ app.py
@@ -18,13 +18,13 @@ peso = quotes_dic["ARS"]["bid"]
 # account_sid = os.environ['TWILIO_ACCOUNT_SID']
 # auth_token = os.environ['TWILIO_AUTH_TOKEN']
 # sender = os.environ['SENDER']
-# account_sid = "ACbade097588007374e1839cb7e55aefc0"
-# auth_token = "44ad68ca6786b057cc78d73b6aa35801"
-# sender = "+13613155385"
+account_sid = "ACbade097588007374e1839cb7e55aefc0"
+auth_token = "44ad68ca6786b057cc78d73b6aa35801"
+sender = "+13613155385"
 
 # instantiating Client, from Twilio
-# client = Client(account_sid, auth_token)
-client = Client()
+client = Client(account_sid, auth_token)
+# client = Client()
 
 # getting phone number list registered in Twilio
 outgoing_caller_ids = client.outgoing_caller_ids.list(limit=20)
@@ -33,7 +33,7 @@ outgoing_caller_ids = client.outgoing_caller_ids.list(limit=20)
 for record in outgoing_caller_ids:
     print('{}: {}'.format(record.friendly_name, record.phone_number))
     message = client.messages.create(
-      from_='+13613155385',
+      from_=sender,
       body=f'Hello, {record.friendly_name}!\n Dollar quotation: R$ {dollar} \n Euro quotation: R$ {euro} \n '
            f'Peso (ARG) quotation: R$ {peso}',
       to=record.phone_number
