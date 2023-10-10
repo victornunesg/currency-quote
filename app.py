d6d84a548efa6e6c1b6bc1dc59cce45745546629
Victor Nunes <victorngomes@gmail.com>
2023-10-10 17:46:46 -0300

    code updated with more quote info, .env file removed

--- app.py
+++ app.py
@@ -1,17 +1,21 @@
 from twilio.rest import Client  # Twilio is the API used to send SMS messages
-from dotenv import load_dotenv
 import requests
-import os
 
 # getting current quotation of all coins
-quotes = requests.get('https://economia.awesomeapi.com.br/json/all')
+quotes = requests.get('https://economia.awesomeapi.com.br/last/BRL-CLP,BRL-ARS,USD-BRL,EUR-BRL')
 quotes_dic = quotes.json()  # creates a quotation python dict using .json() method
 
-dollar = quotes_dic["USD"]["bid"]
-euro = quotes_dic["EUR"]["bid"]
-peso = quotes_dic["ARS"]["bid"]
+peso_chi = quotes_dic["BRLCLP"]["bid"]
+peso_arg = quotes_dic["BRLARS"]["bid"]
+dollar = quotes_dic["USDBRL"]["bid"]
+euro = quotes_dic["EURBRL"]["bid"]
 
-client = Client()
+peso_chi_name = quotes_dic["BRLCLP"]["name"]
+peso_arg_name = quotes_dic["BRLARS"]["name"]
+dollar_name = quotes_dic["USDBRL"]["name"]
+euro_name = quotes_dic["EURBRL"]["name"]
+
+client = Client("ACbade097588007374e1839cb7e55aefc0", "95b2c09867501d18e792eb91fb15af4e")
 
 # getting phone number list registered in Twilio
 outgoing_caller_ids = client.outgoing_caller_ids.list(limit=20)
@@ -21,8 +25,8 @@ for record in outgoing_caller_ids:
     print('{}: {}'.format(record.friendly_name, record.phone_number))
     message = client.messages.create(
       from_="+13613155385",
-      body=f'Hello, {record.friendly_name}!\n Dollar quotation: R$ {dollar} \n Euro quotation: R$ {euro} \n '
-           f'Peso (ARG) quotation: R$ {peso}',
+      body=f'Hello, {record.friendly_name}!\n {dollar_name}: R$ {dollar} \n {euro_name}: R$ {euro} \n '
+           f'{peso_arg_name}: {peso_arg} pesos \n {peso_chi_name}: {peso_chi} pesos',
       to=record.phone_number
     )
     print(message.status)
